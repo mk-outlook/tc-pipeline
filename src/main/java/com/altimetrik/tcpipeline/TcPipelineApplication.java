cf6f77b8974f8bc1273347bf34795e802c10fb59
mk-outlook <manish.khatwani@outlook.com>
2020-07-17 03:33:30 -0400

    Adding Spring demo project

---
+++ src/main/java/com/altimetrik/tcpipeline/TcPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.tcpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altimetrik/tcpipeline/TcPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.tcpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
