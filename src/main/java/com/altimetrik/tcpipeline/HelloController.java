c6e6b01e45e6eb7e171e468fbca7d7c0d36ecf52
mk-outlook <60869395+mk-outlook@users.noreply.github.com>
2020-07-22 18:09:07 +0530

    import RestController and RequestMapping
    import org.springframework.web.bind.annotation.RestController;
    import org.springframework.web.bind.annotation.RequestMapping;

--- src/main/java/com/altimetrik/tcpipeline/HelloController.java
+++ src/main/java/com/altimetrik/tcpipeline/HelloController.java
@@ -1,5 +1,7 @@
 package com.altimetrik.tcpipeline;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
